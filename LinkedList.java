c2288b743f56d421c6987183534d4521fb666e00
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-08-12 13:00:32 +0530

    adede isPalindrome function for a LL

--- LinkedList.java
+++ LinkedList.java
@@ -147,6 +147,67 @@ class LinkedListCreator{
         }
         head = prev;
     }
+
+    public void removeNthFromEnd(int idx){
+        // nth index from end is sz - n + 1 from begining;
+        int sz = 0;
+        int idxAt = 0;
+        Node temp = head;
+        while(temp != null){
+            temp = temp.next;
+            sz++;
+        }
+        temp = head;
+        // traverse till idx sz-n-1 idx and change its next to next's next
+        while(idxAt < sz-idx-1){
+            temp = temp.next;
+            idx++;
+        }
+
+        temp.next = temp.next.next;
+        return;
+    }
+
+    public Node findMid(){
+        Node slow = this.head;
+        Node fast = this.head;
+        while(fast != null && fast.next != null){
+            slow = slow.next; //+1
+            fast = fast.next.next; //+2
+        }
+        return slow; // slow is midNode
+    } 
+    public boolean palindromeLlOrNot(){
+        if(head == null || head.next == null){
+            return true;
+        }
+        //step1 - find mid;
+        Node midNode = this.findMid();
+
+        //step2 - reverse the 2nd half
+        Node prev = null;
+        Node curr = midNode;
+        Node next;  
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev; // right half head dhyaan se soch
+        Node left = head;
+
+        //step3 - check left half and right half traversal
+        while(right != null){
+            if(left.data != right.data){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+
+    }
 }
 
     
@@ -172,7 +233,19 @@ public class LinkedList{
         
         ll1.reverseLL();
         ll1.printLL();
+        ll1.removeNthFromEnd(3);
+        ll1.reverseLL();
+        ll1.printLL();
+        ll2.addFirst(1);
+        ll2.addFirst(2);
+        ll2.addFirst(2);
+        ll2.addFirst(3);
+        ll2.addFirst(2);
+        ll2.addFirst(2);
+        ll2.addFirst(1);
+        System.out.println(ll2.palindromeLlOrNot());
         
+        // great success!
         
 
     }
