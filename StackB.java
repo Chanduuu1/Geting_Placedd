3e965fd08db8fa5bc5ec8a51fb800aea214b67a1
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-08-14 17:31:34 +0530

    stock span problem added

--- StackB.java
+++ StackB.java
@@ -15,9 +15,9 @@ class stackUsingAL{
         if(isEmpty()){
             return -1;
         }
-        int top = list.get(list.size() - 1);
+        int peek = list.get(list.size() - 1);
         list.remove(list.size() - 1);
-        return top;  // returns the element that has been just removed
+        return peek;  // returns the element that has been just removed
     }
 
     public int peek(){
@@ -52,7 +52,7 @@ class stackUsingLL{
         }
         newNode.next = head;
         head = newNode;
-        // right now head is pointing at the top of the stack
+        // right now head is pointing at the peek of the stack
     }
 
     public int pop(){
@@ -120,15 +120,42 @@ public class StackB{
         }
     }
 
-    public static void main(String[] args){
+    public static void printarr(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void stockSpanProb(int[] price){
         Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        reverseStack(s);
-        showStack(s);
-        
+        int span[] = new int[price.length];
+        span[0] = 1;
+        s.push(0);
+        for(int i = 1; i < price.length; i++){
+            if(price[i] < price[s.peek()]){
+                span[i] = i - s.peek();
+                s.push(i);
+            }
+            else if(price[i] >= price[s.peek()]){
+                while(!s.isEmpty() && price[i] >= price[s.peek()]){
+                    s.pop();
+                }
+                if(s.isEmpty()){
+                    span[i] = i + 1;
+                }else{
+                    span[i] = i - s.peek();
+                    s.push(i);
+                }
+                
+            }
 
+        }
+        printarr(span);
         
+    }
+
+    public static void main(String[] args){
+        int[] price = {100,80,60,70,60,85,100};
+        stockSpanProb(price);
+         
     }
 }
