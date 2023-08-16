a1c9eacb847dfc5b20e33761b380ad714583c039
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-08-16 16:05:21 +0530

    added print non repeating letter function

--- QueueB.java
+++ QueueB.java
@@ -226,18 +226,32 @@ class QueueUsing2Stk{
 }
 
 public class QueueB{
-    
+    public static void printNonRepeat(String str){
+        int freq[] = new int[26]; //'a'-'z'
+        Queue<Character> q = new LinkedList<>();
+
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+
+            while(!q.isEmpty() && freq[q.peek()-'a'] > 1){
+                q.remove();
+            }
+
+            if(q.isEmpty()){
+                System.out.print("-1 ");
+            }
+            else{
+                System.out.print(q.peek() + " ");
+                
+            }
+        }
+        System.out.println();
+        
+    }
 
     public static void main(String args[]){
-        QueueUsing2Stk q = new QueueUsing2Stk();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        while(!q.isEmpty()){
-            System.out.println(q.peek());
-            System.out.println(q.remove());
-            
-        }
-
+        printNonRepeat("aabccxb");
     }
 }
