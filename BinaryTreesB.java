0723512ac83960bbbdf2917dceee9a588fc42eff
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-08-18 23:20:25 +0530

    added BT creator and preorder traversal functions

---
+++ BinaryTreesB.java
@@ -0,0 +1,50 @@
+class BinaryTreess{
+    // unless and until the main class is not static you need not worry!!
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    static int idx = -1;
+    public static Node buildTree(int nodes[]){
+        idx++;
+        if(nodes[idx] == -1){
+            return null; // you could also have created a node and made it null but why to do that?
+        }
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+
+        return newNode; // dhyaan se soch this newNode is the root node of the tree
+    }
+
+    public static void preorderTra(Node root){
+        if(root.left == null || root.right == null){
+            return;
+        }
+        System.out.println(root.val + "->");
+        preorderTra(root.left);
+        preorderTra(root.right);
+        
+        
+    }
+}
+
+public class BinaryTreesB{  
+
+    public static void main(String args[]){
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTreess bT1 = new BinaryTreess();
+        
+        int rootValue = bT1.buildTree(nodes).data; // this will build the tree and then also return the root node ka value
+        System.out.println(rootValue);
+        
+    }
+    
+}
