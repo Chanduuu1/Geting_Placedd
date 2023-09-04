5cbcb123bf7277e87512293999aa62fc1ba5d5ea
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-04 12:41:15 +0530

    added weakest soldier code, check the comaparableTo function that's new here

--- HeapB.java
+++ HeapB.java
@@ -151,25 +151,55 @@ public class HeapB{
 
 
 
-    // Rope joining question written inside main function
+
+
+    // first k - weakest soldier row
+    static class Row implements Comparable<Row>{
+        int soldier;
+        int idx;
+
+        public Row(int soldier,int idx){
+            this.soldier = soldier;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Row r2){
+            if(this.soldier == r2.soldier){
+                return this.idx - r2.idx;
+            }
+            else{
+                return this.soldier - r2.soldier;
+            }
+        }
+    }
+
+
+
+
+
+    // first k - weakest soldier row
     public static void main(String args[]){
-        int ropes[] = {2,3,3,4,6};
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i = 0; i < ropes.length; i++){
-            pq.add(ropes[i]);
-        }
+        int army[][] = {{1,0,0,0},
+                        {1,1,1,1},
+                        {1,0,0,0},
+                        {1,0,0,0}};
+        int k = 2;
+        PriorityQueue<Row> pq= new PriorityQueue<>();
 
-        int cost = 0;
-        while(pq.size() > 1){
-            // find the 2 min len in pq at any given iterations.
-            int min1 = pq.remove();
-            int min2 = pq.remove();
-            cost += min1 + min2;
-            pq.add(min1+min2);
-        }
+        // first lets feed into the values of army into row format into PQ
+        for(int i = 0; i <army.length;i++){
+            int count = 0;
+            for(int j = 0; j < army[0].length; j++){
+                count += army[i][j] == 1 ? 1 : 0;
 
-        System.out.println("Minimum cost of joining  ropes is "+ cost);
-        
+            }
+            pq.add(new Row(count, i));
+        }
+        for(int i = 0; i < k; i++){
+            System.out.println("R" + pq.remove().idx);
+            
+        }       
 
     }
 }
