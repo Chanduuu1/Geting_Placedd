b97622c591a5e046efc90da1cc6ee0edf23eca83
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-12 20:17:11 +0530

    added searching in trie funcion

--- TriesB.java
+++ TriesB.java
@@ -27,12 +27,33 @@ public class TriesB{
         curr.eow = true;
     }
 
+    public static boolean search(String key){ // O(L)
+        Node curr = root;
+        for(int lvl = 0; lvl < key.length(); lvl++){
+            int idx = key.charAt(lvl) - 'a';
+            if(curr.children[idx] == null){
+                // this means ch doeasnt exists
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        
+        return curr.eow == true; // if its trur it will ret true else...
 
+    }
 
     public static void main(String[] args){
         String words[] = {"the", "a", "there", "their", "any", "thee"};
         for(int i = 0; i < words.length; i++){
             insert(words[i]);
         }
+
+        System.out.println(search("thee"));
+        System.out.println(search("thor"));
+        System.out.println(search("any"));
+        System.out.println(search("an"));
+
+        
+        
     }
 }
