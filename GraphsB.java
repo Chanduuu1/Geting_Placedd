7a4b19478666291114a397ded0366e77a430397f
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-19 16:33:03 +0530

    added bfs traversal and a create graph function

--- GraphsB.java
+++ GraphsB.java
@@ -12,55 +12,69 @@ public class GraphsB{
         }
     }
 
+    static void createGraph(ArrayList<Edge> graph[]){
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0,1,1));
+        graph[0].add(new Edge(0,2,1));
+
+        graph[1].add(new Edge(1,0,1));
+        graph[1].add(new Edge(1,3,1));
+
+        graph[2].add(new Edge(2,0,1));
+        graph[2].add(new Edge(2,4,1));
+
+        graph[3].add(new Edge(3,1,1));
+        graph[3].add(new Edge(3,4,1));
+        graph[3].add(new Edge(3,5,1));
+
+        graph[4].add(new Edge(4,2,1));
+        graph[4].add(new Edge(4,3,1));
+        graph[4].add(new Edge(4,5,1));
+
+        graph[5].add(new Edge(5,3,1));
+        graph[5].add(new Edge(5,4,1));
+        graph[5].add(new Edge(5,6,1));
+
+        graph[6].add(new Edge(6,5,1));
+
+    }
+
+
+    public static void bfs(ArrayList<Edge>[] graph){
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[graph.length]; // visited array bana li
+        // default adding source node
+        q.add(0);
+
+        // main funda
+        while(!q.isEmpty()){
+            // step1 usse q se nikalna
+            int curr = q.remove();
+
+            if(!vis[curr]){
+                // step 2 - abh kyuki wo visited nahi tha, toh hum usse print karwa denge and uska vis status T  kardenge
+                System.out.print(curr + " ");
+                vis[curr] = true;
+                //step 3 - curr ke saare neighbour ko add to q
+                for(int i = 0; i < graph[curr].size(); i++){ // dekh bhai aaray ke andar edge stored hai ye edge stored hone ka ds, AL hai. Confusion hai toh aaram se feel kar :)
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+                               
+            }
+
+            // dont panic aaram se le
+        }
+    }
+
     public static void main(String args[]){
-        /* 
-                (5)
-            0 ----- 1
-                   / \
-               (1)/   \  (2)
-                 /     \ 
-                2-------3
-                |  (1)
-            (2) |  
-                |
-                4
-        
-         */
-
-        int V = 5;
+        int V = 7;
         ArrayList<Edge>[] graph = new ArrayList[V]; // array of arraylist array ka naam graph hai and uske andar ds AL hai - dekha kya khel khela!?
-        for(int i = 0; i < V; i++){
-            graph[i] = new ArrayList<>(); // array ke har box ke andar jakr we gave life to the AL
-        }
-
-        // 0's vertex
-        graph[0].add(new Edge(0,1,5));
-
-        // 1 vertex
-        graph[1].add(new Edge(1,1,5));
-        graph[1].add(new Edge(1,2,1));
-        graph[1].add(new Edge(1,3,2));
-
-        //2 vertex
-        graph[2].add(new Edge(2,1,1));
-        graph[2].add(new Edge(2,3,1));
-        graph[2].add(new Edge(2,4,2));
-
-        //3 vertex
-        graph[3].add(new Edge(3,2,1));
-        graph[3].add(new Edge(3,1,2));
-
-        //4 vertex
-        graph[4].add(new Edge(4,2,2));
-
-
-        //  now if i want to find 2's neighbour
-        for(int i = 0; i < graph[2].size(); i++){
-            Edge e = graph[2].get(i);
-            // sara details transfered to e!
-            System.out.println(e.dest);
-            
-        }
+        createGraph(graph);
+        bfs(graph);
 
     }
 }
