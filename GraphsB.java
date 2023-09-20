2177b7f6a94a4e6dfccb8e68b8a6948ec37499bb
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-20 23:21:30 +0530

    added cycle exists or not? in a undirected graph using dfs, function

--- GraphsB.java
+++ GraphsB.java
@@ -170,13 +170,87 @@ public class GraphsB{
     }
 
 
+    // thoda compli hai shekhar, but aaram se karna samaj jaega!
+    static void createGraph2(ArrayList<Edge> graph[]){
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0,1,1));
+        graph[0].add(new Edge(0,2,1));
+        graph[0].add(new Edge(0,3,0));
+
+        graph[1].add(new Edge(1,0,1));
+        graph[1].add(new Edge(1,2,1));
+
+        graph[2].add(new Edge(2,0,1));
+        graph[2].add(new Edge(2,1,1));
+
+        graph[3].add(new Edge(3,0,1));
+        graph[3].add(new Edge(3,4,1));
+        
+        graph[4].add(new Edge(4,3,1));
+    }
+    public static boolean detectCycleUn(ArrayList<Edge>[] graph){
+        boolean vis[] = new boolean[graph.length];
+        for(int i = 0; i < graph.length; i++){// this will manage ki koi disjoint hai toh udar bhi traversal ho
+            if(!vis[i]){
+                if(detectCycleUn_Util(graph, vis, i, -1)){  // yeh ek particular disjoint componennt me dfs krkr pata lagagea ki cycle h ya nahi, -1 liya pata nahi time nahi mila sochne ko
+                    return true; // iss functn ne true ret kiya to ret true, thoda bounce hua hoga but dry run, you can do it shekhar!
+                }
+            }
+        }
+
+        return false; // agar kahi se kuch nahi mila toh (saare components me cycle chaan marne ka baad)
+    }
+    public static boolean detectCycleUn_Util(ArrayList<Edge>[] graph, boolean[] vis, int curr, int parent){
+        vis[curr] = true;
+
+        for(int i = 0; i < graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            // case 3
+            if(!vis[e.dest]){
+                //toh.. kuch nahi try to detect cycle aage by dfs traversing ->
+                if(detectCycleUn_Util(graph,vis,e.dest,curr)){ // dekh yaha curr ko par bana diya
+                    return true;
+                }
+            }
+
+            // case 1 - neigh visited and not parent!
+            else if(vis[e.dest] && e.dest != parent){
+                return true;
+            }
+
+            //  case 2 -> eat 5 star, do nothing
+        }
+
+        return false;
+    }
+    
+
 
     public static void main(String args[]){
-        int V = 7;
+        /*
+                    0 -------- 3
+                   /|          |
+                  / |          |
+                 /  |          |
+                1   |          |
+                 \  |          |   
+                  \ |          |
+                   \|          |
+                    2          4
+         
+         
+         */ 
+        
+        
+        
+        
+        int V = 5;
         ArrayList<Edge>[] graph = new ArrayList[V]; // array of arraylist array ka naam graph hai and uske andar ds AL hai - dekha kya khel khela!?
-        createGraph(graph);
-        boolean[] visited = new boolean[V];
-        System.out.println(hasPath(graph, 0, 7,visited));
+        createGraph2(graph);
+        System.out.println(detectCycleUn(graph));
         
 
     }
