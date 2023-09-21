73946b670d82642b1de8338359abf6308735c1db
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-22 01:07:20 +0530

    added all paths from src to dest problem

--- Graphs2B.java
+++ Graphs2B.java
@@ -69,10 +69,44 @@ public class Graphs2B{
 
     }
 
+
+    // all paths from src to target
+    static void createGraph6(ArrayList<Edge> graph[]){
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0,3,1));
+        
+        graph[2].add(new Edge(2,3,1));
+        
+        graph[3].add(new Edge(3,1,1));
+
+        graph[4].add(new Edge(4,0,1));
+        graph[4].add(new Edge(4,1,1));
+
+        graph[5].add(new Edge(5,0,1));  
+        graph[5].add(new Edge(5,2,1));
+    
+    }
+    public static void allPaths(ArrayList<Edge>[] graph, int curr, int dest, String path){
+        if(curr == dest){
+            System.out.println(path +dest);
+            return;
+        }
+
+        for(int i = 0; i < graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            allPaths(graph, e.dest, dest, path+curr);
+        }   
+
+    }
+
+
     public static void main(String args[]){
         int V = 6;
         ArrayList<Edge>[] graph = new ArrayList[V]; 
-        createGraph5(graph);
-        kahnsTopologicalSort(graph);
+        createGraph6(graph);
+        allPaths(graph,5,1,"");
     }
 }
