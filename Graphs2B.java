c647d00d7365ba8afb34593f1f22ec0873615c97
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-22 12:23:47 +0530

    added dijkstra's algo

--- Graphs2B.java
+++ Graphs2B.java
@@ -103,10 +103,94 @@ public class Graphs2B{
     }
 
 
+
+    // Dijkstras
+    static void createGraph7(ArrayList<Edge> graph[]){
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0,1,2));
+        graph[0].add(new Edge(0,2,4));
+        
+        graph[1].add(new Edge(1,3,7));
+        graph[1].add(new Edge(1,2,1));
+
+        graph[2].add(new Edge(2,4,3));
+        
+        graph[3].add(new Edge(3,5,1));
+
+        graph[4].add(new Edge(4,3,2));
+        graph[4].add(new Edge(4,5,5));
+
+    
+    }
+
+    static class Pair implements Comparable<Pair>{
+        int vertex;
+        int distToVertex;
+
+        public Pair(int v, int d){
+            this.vertex = v;
+            this.distToVertex = d;
+        } 
+
+        @Override
+        public int compareTo(Pair p2){
+            return this.distToVertex - p2.distToVertex; // this will create min heap of pairs on the basis of dist
+        }
+    }
+
+    public static void dij(ArrayList<Edge> graph[], int src){ // O(V + E logV)
+        int[] dis = new int[graph.length];
+        for(int i = 0; i < graph.length; i++){
+            if(i != src){
+                dis[i] = Integer.MAX_VALUE; //+infinity
+            }
+        }
+         boolean[] vis = new boolean[graph.length];
+         PriorityQueue<Pair> pq = new PriorityQueue<>();
+         pq.add(new Pair(src, 0));
+         // loop
+         while(!pq.isEmpty()){
+            // yaha pq ka function itna hi hai ki jo neighbour me jaa rahe hai, unme se sabse kam dist kiska hai
+            // kam dist wala node top pr hoga
+            // so removing it will give me that
+            Pair curr = pq.remove();
+
+            // kya curr ke neigbours visited hai?
+            if(!vis[curr.vertex]){
+                vis[curr.vertex] = true;
+                // neighbour
+                for(int i = 0; i < graph[curr.vertex].size(); i++){
+                    Edge e = graph[curr.vertex].get(i);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+
+                    
+                    if(dis[u] + wt < dis[v]){ // update dist
+                        dis[v] = dis[u] + wt;
+                        pq.add(new Pair(v, dis[v]));
+                    }
+                }
+            }
+         }
+
+         // printing all dist
+         for(int i = 0; i < dis.length; i++){
+            System.out.print(dis[i] + " ");
+            
+         }
+    }
+
+
+
     public static void main(String args[]){
         int V = 6;
         ArrayList<Edge>[] graph = new ArrayList[V]; 
-        createGraph6(graph);
-        allPaths(graph,5,1,"");
+        createGraph7(graph);
+        dij(graph,0);
     }
 }
