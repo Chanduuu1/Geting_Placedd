53b846ff7d6b7d6527beafc6e65546302bea40da
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-22 13:41:35 +0530

    added bellman Ford's algo

--- Graphs2B.java
+++ Graphs2B.java
@@ -187,10 +187,68 @@ public class Graphs2B{
 
 
 
+    //bellman ford
+    static void createGraph8(ArrayList<Edge> graph[]){
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0,1,2));
+        graph[0].add(new Edge(0,2,4));
+        
+        graph[1].add(new Edge(1,2,-4));
+
+        graph[2].add(new Edge(2,3,2));
+        
+        graph[3].add(new Edge(3,4,4));
+
+        graph[4].add(new Edge(4,1,-1));
+
+    
+    } 
+    public static void bellmanFord(ArrayList<Edge>[] graph, int src){
+        int dist[] = new int[graph.length];
+        for(int i = 0; i < dist.length; i++){
+            if(i!= src){
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        // main algo
+
+        int V = graph.length;
+        for(int i = 0; i < V-1; i++){ // O(VE) cube sab mat samajna abh thoda pro ban gaya hai
+            for(int j = 0; j < graph.length; j++){
+                for(int k = 0; k < graph[j].size(); k++){
+                    Edge e = graph[j].get(k);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    // relaxation
+                    if(dist[u] != Integer.MAX_VALUE /* yeh isiliye likha hai ki incase kisi edge u->v pr u infi ho toh 
+                    usme kuch bhi add karenege to kuch problem ho jaegi so yeh dhyan rakhna */ && dist[u] + wt < dist[v]){
+                        dist[v] = dist[u] + wt;
+
+                    }
+
+                }
+            }
+        }
+
+
+        // now print
+        for(int i = 0 ; i < dist.length; i++){
+            System.out.print(dist[i] + " ");    
+        }
+    }
+
+
+
     public static void main(String args[]){
         int V = 6;
         ArrayList<Edge>[] graph = new ArrayList[V]; 
-        createGraph7(graph);
-        dij(graph,0);
+        createGraph8(graph);
+        bellmanFord(graph,0);
     }
 }
