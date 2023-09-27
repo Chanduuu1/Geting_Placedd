d42409ef8850ab052be108c18fc409a6d999438b
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-27 12:15:54 +0530

    added climbing stairs normal rec + with memoization

---
+++ DP.java
@@ -0,0 +1,34 @@
+public class DP{
+    
+    public static int countWays(int n){
+        // base case
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        return countWays(n-1) + countWays(n-2); 
+    }
+    public static int countWaysMemoized(int n, int ways[]){
+        // base case
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        if(ways[n] != 0){
+            return ways[n]; /// will return ways till nth step directly instead of rec
+        }
+        ways[n] = countWaysMemoized(n-1,ways) + countWaysMemoized(n-2,ways); 
+        return ways[n];
+    }
+
+    public static void main(String args[]){
+        int n=5;
+        int ways[] = new int[n+1];
+        System.out.println(countWaysMemoized(n,ways) + " ways");
+        
+    }
+}
