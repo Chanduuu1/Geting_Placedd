7909eb6a44b7c02497338521433085b288bc0ccb
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-30 00:09:57 +0530

    added LIS

--- DP.java
+++ DP.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class DP{
     
 
@@ -337,12 +338,61 @@ public class DP{
 
 
 
-    public static void main(String args[]){
-        String str1 = "abcdge";
-        String str2 = "abedg";
-        int n = str1.length();
-        int m = str2.length();
+    // LIS
+    public static int lis(int arr[]){
+        // for unique elements, platforms me sab corner cases jesa hota hai jaha duplicate de dete hai values
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < arr.length; i++){
+            set.add(arr[i]);
+        }
 
-        System.out.println(lcsubStr(str1, str2));        
+        int arr2[] = new int[set.size()]; 
+        int i = 0;
+        for(int num : set){
+            arr2[i] = num;
+            i++;
+        } // sare unique elemsnts ko store karwa diya
+
+        Arrays.sort(arr2); // ascending
+
+        return lcsOnArr(arr, arr2);
+    }
+    public static int lcsOnArr(int arr1[], int arr2[]){
+        int n = arr1.length;
+        int m = arr2.length;
+        
+        int dp[][] = new int[n+1][m+1];
+        // initialize with 0 base case
+        for(int i = 0; i < n+1; i++){
+            for(int j = 0; j < m+1; j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        //fill
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < m+1; j++){
+                if(arr1[i-1] == arr2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else{
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] = Math.max(ans1,ans2);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+    
+
+
+    public static void main(String args[]){
+        int arr[] = {50,3,10,7,40,80};
+        // ANS = 4
+        System.out.println(lis(arr));        
     }
 }
