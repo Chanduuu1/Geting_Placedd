5c2605430d4e8f2b7eb5d8a74be5d5c8840d404c
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-29 23:34:59 +0530

    added lc substring problem with tabulation approach

--- DP.java
+++ DP.java
@@ -304,6 +304,37 @@ public class DP{
 
 
 
+    //longest common subStirng
+    public static int lcsubStr(String str1, String str2){
+        int n = str1.length();
+        int m = str2.length();
+        int finalAns = 0;
+
+        int dp[][] = new int[n+1][m+1];
+        // initialize with 0 base case
+        for(int i = 0; i < n+1; i++){
+            for(int j = 0; j < m+1; j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        //fill
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < m+1; j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    finalAns = Math.max(finalAns, dp[i][j]); // kya muje naya longest mila? yeh check karne ke liye
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return finalAns;
+    }
+
 
 
     public static void main(String args[]){
@@ -312,6 +343,6 @@ public class DP{
         int n = str1.length();
         int m = str2.length();
 
-        System.out.println(lcsTabluation(str1, str2));        
+        System.out.println(lcsubStr(str1, str2));        
     }
 }
