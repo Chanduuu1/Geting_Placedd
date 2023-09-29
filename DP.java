2ef89cf48be5282a0e95bb012f403a97ef209662
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-29 22:00:48 +0530

    added lcs with memoization dp

--- DP.java
+++ DP.java
@@ -250,13 +250,41 @@ public class DP{
             return Math.max(ans1,ans2);
         }
     }
+    //lcs memoization
+    public static int lcsMemoization(String str1,  int n, String str2, int m, int dp[][]){
+        if(n == 0 || m == 0){
+            return 0;
+        }
 
+        if(dp[n][m] != -1){
+            return dp[n][m];
+        }
+        if(str1.charAt(n-1) == str2.charAt(m-1)){
+            dp[n][m] = lcsMemoization(str1,n-1,str2,m-1,dp) + 1; // same wali case
+            return dp[n][m];
+        }
+        else{
+            int ans1 = lcsMemoization(str1, n-1, str2,m,dp);
+            int ans2 = lcsMemoization(str1, n, str2,m-1,dp);
+            dp[n][m] = Math.max(ans1,ans2);
+            return dp[n][m];
+        }
+    }
 
     public static void main(String args[]){
         String str1 = "abcdge";
         String str2 = "abedg";
-        System.out.println(lcs(str1, str1.length(), str2, str2.length()));
-        
-        
+        int n = str1.length();
+        int m = str2.length();
+
+        int dp[][] = new int[n+1][m+1];
+        // initialize with -1
+        for(int i = 0; i < n+1; i++){
+            for(int j = 0; j < m+1; j++){
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(lcsMemoization(str1, str1.length(), str2, str2.length(),dp));        
     }
 }
