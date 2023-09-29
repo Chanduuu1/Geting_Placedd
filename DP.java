41bbdf5a00be6c2b133ca6d393cc4cd5ef57e02d
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-30 01:32:19 +0530

    added edit distance code

--- DP.java
+++ DP.java
@@ -390,9 +390,51 @@ public class DP{
     
 
 
+    // edit distance
+    public static int editDistance(String s1, String s2){
+        int n = s1.length();
+        int m = s2.length();
+        int dp[][] = new int[n+1][m+1];
+
+        // initalize
+        for(int i = 0; i < n+1; i++){
+            for(int j = 0; j < m+1; j++){
+                if(i == 0){
+                    dp[i][j] = j;
+                }
+                if(j == 0){
+                    dp[i][j] = i;
+                }
+            }
+        }
+
+        //bottom up step
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < m+1; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int add = dp[i][j-1] + 1;
+                    int del = dp[i-1][j] + 1;
+                    int rep = dp[i-1][j-1] + 1;
+                    dp[i][j] = Math.min(add,Math.min(del,rep));
+
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+
+
     public static void main(String args[]){
-        int arr[] = {50,3,10,7,40,80};
-        // ANS = 4
-        System.out.println(lis(arr));        
+        String word1 = "intention";
+        String word2 = "execution";
+
+        System.out.println(editDistance(word1,word2));
+        
+
     }
 }
