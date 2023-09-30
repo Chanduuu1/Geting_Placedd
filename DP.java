fced219e63474c5df84488a4c7dc515cccb68415
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-30 23:37:27 +0530

    added minimum partioning Q

--- DP.java
+++ DP.java
@@ -627,10 +627,44 @@ public class DP{
     }
 
 
+
+    // Minimum partioning question
+    public static int minPartition(int arr[]){
+        int n = arr.length;
+        int sum = 0;
+        for(int i = 0; i <arr.length; i++){
+            sum += arr[i];
+        }
+
+        // W = sum/2
+        int W = sum/2;
+
+        int dp[][] = new int[n+1][W+1];
+        // skinpping the initialization step because no use in java, inbuilt 0 hi hota
+
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < W+1; j++){
+                if(arr[i-1] <= j){ // valid
+                    dp[i][j] = Math.max(arr[i-1] + dp[i-1][j-arr[i-1]], dp[i-1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        int sum1 = dp[n][W];
+        int sum2 = sum - sum1;
+        return Math.abs(sum1-sum2);
+
+    }
+
+
+
+
     public static void main(String args[]){
-        int[] arr = {1,2,3,4,3};
-        
-        System.out.println(mcmTab(arr));
+        int[] arr = {1,6,11,5};
+        System.out.println(minPartition(arr));
         
         
 
