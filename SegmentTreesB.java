626d9d7462aa037485ababd3c2e5c90ac73eedbc
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-10-01 02:26:22 +0530

    added segment tree updation code

--- SegmentTreesB.java
+++ SegmentTreesB.java
@@ -35,11 +35,33 @@ public class SegmentTreesB{
             return left + right;
         }
     }
-
     public static int getSum(int arr[], int qi, int qj){
         int n = arr.length;
         return getSumUtil(0,0,n-1,qi,qj);
     }
+
+    public static void updateUtil(int i, int si, int sj, int idx, int diff){
+        if(idx > sj || idx < si){
+            return;
+        }
+        tree[i] += diff;
+        if(si != sj){ //non leaf
+            int mid = (si+sj)/2;
+            updateUtil(2*i+1, si, mid, idx, diff); // left
+            updateUtil(2*i+2, mid+1, sj, idx, diff); // right
+        }
+    }
+    public static void update(int arr[], int idx, int newVal){
+        int n = arr.length;
+        int diff = newVal - arr[idx];
+        arr[idx] = newVal;
+
+        updateUtil(0, 0, n-1, idx, diff);
+    }
+
+
+
+
     
     public static void main(String args[]){
         int arr[] = {1,2,3,4,5,6,7,8};
@@ -47,12 +69,8 @@ public class SegmentTreesB{
         init(n);
         buildST(arr,0,0,n-1);
 
-        // priniting tree
-        for(int i = 0; i < tree.length; i++){
-            System.out.print(tree[i]+" ");
-            
-        }
-
+        System.out.println(getSum(arr,2,5));
+        update(arr,2,2);
         System.out.println(getSum(arr,2,5));
     
     }
