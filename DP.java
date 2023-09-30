843a5a24e8044a9d38f9d0b1863294266e61f3cc
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-09-30 16:50:10 +0530

    added CATALAN using memo

--- DP.java
+++ DP.java
@@ -488,12 +488,31 @@ public class DP{
 
         return ans;
     }
+    // catlan number using memoization
+    public static int catalanMem(int n, int[]dp){
+        if(n==0 || n ==1){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int ans = 0; // Cn
+        for(int i = 0; i <= n -1; i++){
+            ans += catalanMem(i,dp) * catalanMem(n-i-1,dp);
+            
+        }
+        dp[n] = ans;
+    
+        return dp[n];
+    }
 
 
 
     public static void main(String args[]){
-        int n = 4;
-        System.out.println(catalanRec(4));
+        int n = 30;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1); // this function initliazes -1 in all indexes of dp
+        System.out.println(catalanMem(n,dp));
         
         
 
