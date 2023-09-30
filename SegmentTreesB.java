e41f7f87b88b38bdd1e7e4275eb8d03ef91b58c5
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-10-01 02:20:02 +0530

    added segment tree query code

--- SegmentTreesB.java
+++ SegmentTreesB.java
@@ -19,6 +19,27 @@ public class SegmentTreesB{
 
         return tree[i]; // optional step
     }
+
+
+    public static int getSumUtil(int i, int si, int sj, int qi, int qj){
+        if(qj <= si || qi >= sj){ //non oveerlap
+            return 0;
+        }
+        else if(si >= qi && sj <= qj){ // cmplt overlap
+            return tree[i];
+        }
+        else{ //partial
+            int mid = (si+sj)/2;
+            int left = getSumUtil(2*i+1, si, mid, qi, qj);
+            int right = getSumUtil(2*i+2, mid+1, sj, qi, qj);
+            return left + right;
+        }
+    }
+
+    public static int getSum(int arr[], int qi, int qj){
+        int n = arr.length;
+        return getSumUtil(0,0,n-1,qi,qj);
+    }
     
     public static void main(String args[]){
         int arr[] = {1,2,3,4,5,6,7,8};
@@ -31,5 +52,8 @@ public class SegmentTreesB{
             System.out.print(tree[i]+" ");
             
         }
+
+        System.out.println(getSum(arr,2,5));
+    
     }
 }
