b151597337966e667310d9ae56791a41f54c901a
Chanduuu1 <shekharkrishnan13@gmail.com>
2023-10-01 01:11:21 +0530

    added segment tree construct code

---
+++ SegmentTreesB.java
@@ -0,0 +1,35 @@
+public class SegmentTreesB{
+    
+    static int tree[];
+
+    public static void init(int n){
+        tree = new int[4*n];
+    }
+
+    public static int buildST(int arr[], int i, int start, int end){
+        if(start == end){
+            tree[i] = arr[start];
+            return arr[start];
+        }
+
+        int mid = (start+end)/2;
+        buildST(arr, 2*i + 1, start, mid); // left subtree - 2*i+1
+        buildST(arr, 2*i + 2, mid+1, end); // right subtree - 2*i+2
+        tree[i]  = tree[2*i+1] + tree[2*i+2];
+
+        return tree[i]; // optional step
+    }
+    
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5,6,7,8};
+        int n = arr.length;
+        init(n);
+        buildST(arr,0,0,n-1);
+
+        // priniting tree
+        for(int i = 0; i < tree.length; i++){
+            System.out.print(tree[i]+" ");
+            
+        }
+    }
+}
